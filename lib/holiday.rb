09d08a9c0b4ea6971401d8a9b4b48229cd31cf3d
Anthea Merton <anthea.merton@gmail.com>
2019-03-25 09:33:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,8 +46,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-holiday_hash[:winter].collect do |key,value|
-  "#{value}"
+holiday_hash[:winter].collect do |key,array|
+  array
 end
 
 def all_supplies_in_holidays(holiday_hash)
